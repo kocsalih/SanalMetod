b1a72d17af12f9cc3c9fcadd5a6a352e1a633616
salihkoc <salihkoc-e@users.noreply.github.com>
2021-12-03 16:59:01 +0300

    Add project files.

---
+++ SanalMetod/Musteri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanalMetod
+{
+    public class Musteri //:object ->Framework tümnesleri object den türetilmiştir
+    {
+
+        public int musteriId { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+
+
+
+
+        public override string ToString()
+        {
+            return isim +"--" + soyisim;
+        }
+
+
+    }
+}
---
+++ SanalMetod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanalMetod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+
+            m1.isim = "Salih";
+            m1.soyisim = "Koç";
+
+            Console.WriteLine(m1.ToString());
+            Console.ReadKey();
+        }
+    }
+}
